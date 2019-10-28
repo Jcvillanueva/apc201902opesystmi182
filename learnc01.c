ec72d205a50c3a1dfef018020646d14d576e63cf
John Paul Villanueva <Jcvillanueva@student.apc.edu.ph>
2019-10-28 10:10:45 +0800

    Create learnc01.c

---
+++ learnc01.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+
+        {
+        char fname[20];
+        char lname[20];
+        printf("Please enter your first name:");
+        scanf("%s",fname);
+        printf("\nPlease Enter your last name:");
+        scanf("%s", lname);
+
+        printf("\nYour First name is %s" ,fname);
+        printf("\nYour Last name is %s\n" , lname);
+        return 0;
+        }
